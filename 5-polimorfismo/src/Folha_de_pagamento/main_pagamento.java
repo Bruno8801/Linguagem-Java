b667def738f857abd78e0783a4e9d756bc98b183
Bruno Rodrigues <bruno.silva881@hotmail.com>
2026-09-20 19:30:31 -0300

    Renomeia pagamento para main_pagamento

--- 5-polimorfismo/src/Folha_de_pagamento/main_pagamento.java
+++ 5-polimorfismo/src/Folha_de_pagamento/main_pagamento.java
@@ -1,11 +1,11 @@
 package Folha_de_pagamento;
-public class pagamento {
+public class main_pagamento {
     public static void main(String[] args) {
         /* Inserindo dados em um array para a criação de uma planilha de dados, utilizando o polimorfismo, que oferece importantes vantagens na linguagem Java. */
         Funcionario[] meusFuncionarios = {
             new Assalariado("Akalih", 10025, 2800),
             new Horista("Yone", 10029, 36, 98),
-            new Comissionado("Jhin", 10042, 180, 35)
+            new Comissionado("Yone", 10042, 180, 35)
         };
 
         Funcionario f;
